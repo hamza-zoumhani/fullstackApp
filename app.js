a590032fd62a542fa23f067da13fda1339fa32fb
Hamza Zoumhani <zoumhanihamza@gmail.com>
2019-10-12 10:17:50 -0500

    more stuff

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose=require('mongoose');
 var app=express()
 
-//DB STUFF
+//DB STUFF=========================================
 mongoose.connect('mongodb://localhost/todo');
 
 var taskSchema=mongoose.Schema({
@@ -20,7 +20,13 @@ db.once('open', function(){
     console.log("Mongoose connected to todo");
 });
 
+//DB STUFF=========================================
+
 app.use('/', express.static('files'));
+app.get('/todo/all', function(req, res){
+    todo.find({}).exec(function(err, data))
+    res.json(data);
+});
 
 app.listen(3000, function(){
     console.log("Express running on port: 3000");
